4c883b2eb916eff80b509c8db023b57dc97dc3ef
ANASTAD2000 <anasbaclibr@gmai.com>
2025-03-12 15:02:38 +0000

    first coommit and add sql file

---
+++ sport.sql
@@ -0,0 +1,18 @@
+create database SportFlow ;
+use SportFlow ;
+
+CREATE table members (
+id int primary key,
+nom varchar (200),
+date_de_naissance date,
+sport_partiqui varchar(2000)
+);
+create table Entraineur (
+nom varchar(200), 
+specialite varchar (200)
+);
+create table Seance (
+
+
+);
+ 
